b39f88d61432803f72d6009e3b4e222ece7d3baf
ettubrutesbro <Jack.Leng@gmail.com>
2015-02-19 02:35:33 -0800

    outline geometry works / optimized
    now for realRotate / outline on select.....

--- web/scripts/3d.js
+++ web/scripts/3d.js
@@ -42,10 +42,10 @@
     var pillarTargets = [{y:2},{y:8},{y:4},{y:12}]
 
     //debug
-    var debugState = 0
-    var debugInfo = document.querySelector('#debug')
-    var debugInput = document.querySelector('#debugInput')
-    var debugButton = document.querySelector('#debugButton')
+    // var debugState = 0
+    // var debugInfo = document.querySelector('#debug')
+    // var debugInput = document.querySelector('#debugInput')
+    // var debugButton = document.querySelector('#debugButton')
     //-----------------------------------------------
     // END GLOBAL VARIABLE DECLARATION
     //-----------------------------------------------
@@ -53,18 +53,14 @@
     setup()
     animate() //render() is nested in here
     initTouchEvents()
-    lightDebug()
-    //cameraMove(false,0,true,{x: -10, y: 8})
+    //lightDebug()
 
     function setup(){
 
       camera.position.set( -19, 13, 20 )
-      //camera.position.set( -19, 0, 20 )
-      //camera.position.set( 0, 13, 0 )
       camera.rotation.order = 'YXZ'
       camera.rotation.y = - Math.PI / 4
       camera.rotation.x = Math.atan( - 1 / Math.sqrt( 2 ) )
-      //camera.rotation.x = 20*(Math.PI/180)
       camera.zoom = .83
       camera.updateProjectionMatrix()
 
@@ -79,57 +75,36 @@
       container.appendChild( renderer.domElement )
 
       //materials for seseme & orb (eventually need multiples for seseme?)
-      var sesememtl = new THREE.MeshPhongMaterial({color: 0x1b1d1e, shininess: 16, specular: 0x413632})
-      sesememtl.shading = THREE.FlatShading
-   
+      var sesememtl = new THREE.MeshPhongMaterial({color: 0x1b1d1e, 
+        shininess: 16, specular: 0x413632})
       var groundmtl = new THREE.MeshBasicMaterial({color: 0x878787})
-      
-      
-      var orbmtl = new THREE.MeshPhongMaterial({color: 0x000000, shininess: 10, specular: 0x878787})
-      var outlinemtl = new THREE.MeshBasicMaterial( { color: 0xff0000, linewidth: 4} )
-
+      var orbmtl = new THREE.MeshPhongMaterial({color: 0x000000, 
+        shininess: 10, specular: 0x878787})
       var hilightmtl = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide })
 
-
       //LIGHTING
-      lighthelpergeo = new THREE.BoxGeometry(3,3,3)
-      targethelpergeo = new THREE.BoxGeometry(1,1,1)
-      helper = new THREE.Mesh(lighthelpergeo, new THREE.MeshNormalMaterial())
-      helper.rotation.set(camera.rotation.x, camera.rotation.y, camera.rotation.z)
-
-      light = new THREE.SpotLight(0xffffff, 1.8)
-      light.add(helper)
-      light.position.set(24,80,20)
-      light.castShadow = true
-      light.shadowDarkness = 0.2
+      backlight = new THREE.SpotLight(0xffffff, 1.8)
+      backlight.position.set(24,80,20)
+      backlight.target.position.set(2,1,0)
+      backlight.castShadow = true
+      backlight.shadowDarkness = 0.2
+      backlight.shadowMapWidth = 768; // default is 512
+      backlight.shadowMapHeight = 768; // default is 512
+      // helper = new THREE.Mesh(box, normalmtl)
+      // helper.rotation.set(same as cam.x,y,z)
+      // light.add(helper)
       // light.shadowCameraVisible = true
-
-      light.target.position.set(2,1,0)
-
-      light.shadowMapWidth = 768; // default is 512
-      light.shadowMapHeight = 768; // default is 512
-
-      seseme.add(light)
+      seseme.add(backlight)
      
-
-      light2 = new THREE.SpotLight(0xffffff, .6)
-      // light2.target.position.set(2,1,0)
-      // light2.castShadow = true
-      // light2.shadowDarkness = 0.5
-
-      //light2.add(helper2)
-      // //light2 = new THREE.AmbientLight(0x1a1a1a, 0)
-      
-      light2.position.set(camera.position.x,camera.position.y-8,camera.position.z)
-      
-      scene.add(light2)
+      camlight = new THREE.SpotLight(0xffffff, .6)
+      camlight.position.set(camera.position.x,camera.position.y-8,camera.position.z)     
+      scene.add(camlight)
 
       // INTERACT setup -- event listener, initializing interact vars
       window.addEventListener( 'mousemove', onMouseMove, false)
       window.addEventListener( 'mouseup', onMouseUp, false)
       window.addEventListener( 'mousedown', onMouseDown, false)
-     window.addEventListener( 'deviceorientation', onDeviceOrient, false)
-     //debugButton.addEventListener('click', lightDebug, false)
+      window.addEventListener( 'deviceorientation', onDeviceOrient, false)
 
       mouseLocation = { x:0, y:0, z:1 }
       raycaster = new THREE.Raycaster()
@@ -137,41 +112,28 @@
       // EXTERNAL LOADING - getting .js 3d models into the canvas
       var pillar1, pillar2, pillar3, pillar4
       loader.load("assets/pedestal.js", function(geometry,evt){
-
         pedestal = new THREE.Mesh(geometry, sesememtl)
         pedestal.applyMatrix( new THREE.Matrix4().makeTranslation(1.5, 0, 1))
-        pedestal.scale.set(0.5,0.5,0.5)
         pedestal.castShadow = true
-        // pedestal.receiveShadow = true
         pedestal.name = "pedestal"
-        pedestal.overdraw = true
         seseme.add(pedestal)
-
       }) 
       loader.load("assets/pillarA.js", function(geometry,evt){
-
         pillar1 = new THREE.Mesh(geometry, sesememtl)
         pillar1.applyMatrix( new THREE.Matrix4().makeTranslation( -5, 0, -5 ) )
-        pillar1.overdraw = true
         pillar1.name = "pillar1"
-        // pillar1.receiveShadow = true
         pillar1.castShadow = true
         pillargroup.add(pillar1)
 
         pillar4 = new THREE.Mesh(geometry, sesememtl)
         pillar4.applyMatrix( new THREE.Matrix4().makeTranslation( 5, 0, -5 ) )
         pillar4.rotation.y = -90 * Math.PI / 180
-        pillar4.overdraw = true
         pillar4.name = "pillar4"
-        // pillar4.receiveShadow = true
         pillar4.castShadow = true
         setTimeout(function(){
           pillargroup.add(pillar4)
         },10) //this is awful and should not be
-
-        //pillar1.add(pillar1b)
       })
-
       loader.load("assets/pillarA_outline.js", function(geometry){
         var pillar1o = new THREE.Mesh(geometry, hilightmtl)
         var pillar4o = new THREE.Mesh(geometry, hilightmtl)
@@ -179,26 +141,28 @@
         pillar4.add(pillar4o)
       })
 
-
       loader.load("assets/pillarB.js", function(geometry,evt){
         pillar2 = new THREE.Mesh(geometry, sesememtl)
         pillar2.applyMatrix( new THREE.Matrix4().makeTranslation( -5, 0, -5 ) )
-        pillar2.overdraw = true
         pillar2.name = "pillar2"
-        // pillar2.receiveShadow = true
         pillar2.castShadow = true
         pillargroup.add(pillar2)
 
         pillar3 = new THREE.Mesh(geometry, sesememtl)
         pillar3.applyMatrix( new THREE.Matrix4().makeTranslation( -5, 0, 5 ) )
         pillar3.rotation.y = 90 * Math.PI / 180
-        pillar3.overdraw = true
-        // pillar3.receiveShadow = true
         pillar3.castShadow = true
         pillar3.name = "pillar3"
         pillargroup.add(pillar3)
-
       })
+      loader.load("assets/pillarB_outline.js", function(geometry,evt){
+        var pillar2o = new THREE.Mesh(geometry, hilightmtl)
+        pillar2.add(pillar2o)
+        var pillar3o = new THREE.Mesh(geometry, hilightmtl)
+        pillar3.add(pillar3o)
+      })
+
+      seseme.add(pillargroup)
 
       //the orb is generated here (adjust segments for smooth)
       var orb = new THREE.Mesh( new THREE.SphereGeometry( 2.5, 7, 5 ), orbmtl )
@@ -207,38 +171,27 @@
       seseme.add(orb)  
 
       //groundplane
-      var ground = new THREE.Mesh(new THREE.PlaneBufferGeometry( 175, 175, 175 ), 
+      var ground = new THREE.Mesh(new THREE.PlaneBufferGeometry( 210, 210, 210 ), 
         groundmtl)
       ground.position.set(0,-17.7,0)
       ground.rotation.x = -90*(Math.PI/180)
       ground.receiveShadow = true
       scene.add(ground)
+      
 
-      console.log(pillargroup)  
-      seseme.add(pillargroup)
+      scene.add(seseme)
 
-
-      scene.add (seseme)
-
-
-        setTimeout(function(){updateValues()},500) //no idea why, but this only
-        // works with a setTimeout that waits (even 10ms is enough) to fire it
-
+      setTimeout(function(){updateValues()},800) //no idea why, but this only
+      // works with a setTimeout that waits (even 10ms is enough) to fire it
     } //end setup
 
     function animate(){ //put 3d animations here
         requestAnimationFrame( animate )
-        //camera.rotation.x+=0.001
-      
         render()
         TWEEN.update()
-
-
       } // end animate
 
-    function render() { //put frame-by-frame checks and operations here
-      //such as RENDERING or checking
-        //DISPLAY
+    function render() { 
         renderer.render( scene, camera )
     } // end render
 
@@ -360,7 +313,14 @@
         rotationDeceleration.easing(TWEEN.Easing.Cubic.Out)
         rotationDeceleration.onComplete(function(){
            
-           
+           var finalRotate = seseme.rotation.y * (180/Math.PI)
+           console.log(seseme.rotation.y * (180/Math.PI))
+
+           if(finalRotate/360 >= 1){
+            var realRotation = finalRotate/(finalRotate/360) 
+            console.log('1+ rotation')
+           }
+
            //something here to SET rotation to a 0-380 val
         })
         rotationDeceleration.start()
@@ -400,24 +360,18 @@
     }
 
     function lightDebug(){
+      light.shadowCameraVisible = true
       if(debugState == 1){
         var newInfo = debugInput.textContent
         newInfo = newInfo.split(" ")
         console.log(newInfo)
-
         light.position.x = newInfo[0]
         light.position.y = newInfo[1]
         light.position.z = newInfo[2]
-
         light.target.position.set(newInfo[3],newInfo[4],newInfo[5])
-        //light.shadowCameraVisible = true
-
-
         debugInfo.textContent = "x:" + light.position.x +
         " y:" + light.position.y + " z:" + light.position.z + 
         " px: " + light.target.position.x + " py:" + light.target.position.y +
         " pz:" + light.target.position.z
-
       }
- 
     }
