94ee756fb786a8b6db65cdb51c98742e00391f10
ettubrutesbro <Jack.Leng@gmail.com>
2015-03-10 17:10:46 -0700

    icons and stuff

--- web/scripts/data_visuals.js
+++ web/scripts/data_visuals.js
@@ -11,11 +11,11 @@ var breakdownMtls = { //material index for breakdown view by resource/metric
 
 var gradeWords = {
 	'Energy Use Intensity': {
-		best: ['sweet building', 'rather efficient', 'building mvp', 'singing birds', 'children: saved'],
-		good: ['pretty efficient','kinda good','mostly acceptable','probably sustainable'],
-		ok: ['mostly alright','kinda mediocre','probably fine','sorta average','barely okay'],
-		bad: ['kinda sucks','kinda harmful', 'not efficient','needs improvement','pretty bad','mostly awful','not good', 'approaching awful', 'moderately gross', 'sad animals'],
-		terrible: ['the worst','actually shit','totally pernicious','actively pernicious','enviro suicide','weeping planet','kinda sickening','dead puppies', 'earth wrecker', 'wasteland imminent', 'actively harmful']
+		best: ['(heavy breathing)','sweet building','totally sweet','rather efficient','building mvp','singing birds','children: saved','example setter','role model','trend setter','building #lifegoals','efficiency amazeballs','*breathes heavily*','efficiency wunderkind','best building',"doesn't waste",'bright hope','best&brightest'],
+		good: ['pretty efficient','kinda good','mostly acceptable','probably sustainable', 'almost there!', 'doing better', 'getting there!', 'not bad','cool building','almost... ..there..','not wasteful'],
+		ok: ['mostly alright','kinda mediocre','probably fine','sorta average','barely okay', 'almost garbage', 'nearly acceptable', 'pretty meh','what ever','mediocre building','okay building','rating: ¯\\_(ツ)_/¯','needs effort','try harder','do better','.... ..ok','basic building','totally average'],
+		bad: ['abyss gazing', 'kinda sucks','kinda harmful', 'not efficient','needs improvement','pretty bad','mostly awful','not good','approaching awful', 'moderately gross', 'sad animals', 'straight garbage', 'nature cries','nature abuse','just no'],
+		terrible: ['literal chamberpot','frightful prospects','wasteful horror',"nature's nemesis",'the worst','actually shit','totally pernicious','actively pernicious','enviro suicide','weeping planet','kinda sickening','please no','dead puppies','earth wrecker','wasteland imminent','actively harmful','post apocalyptic','doves cry','wholly garbage','entirely shameful','earth killer','waste incarnate','see image']
 	},
 	'Gallons Per Minute': [
 
